93f64c37337d8dcee98c69866c15d6a7a17e9686
JATIN ANEJA <W9359412@live.tees.ac.uk>
2021-09-27 11:52:52 +0100

    staff_increment

--- Program.cs
+++ Program.cs
@@ -4,6 +4,28 @@ namespace Employee
 {
     class Program
     {
+
+
+        public static String staff_increment(Double hrs)
+        {
+            if (hrs > 40)
+            {
+                Double wage = ((9.50 * 40) + (14.25 * (hrs - 40)));
+                wage = Math.Round(wage, 4, MidpointRounding.AwayFromZero);
+                String result = string.Format("{0:F2}", wage);
+                return result;
+            }
+            else
+            {
+                Double wage = hrs * 9.50;
+                wage = Math.Round(wage, 4, MidpointRounding.AwayFromZero);
+                String result = string.Format("{0:F2}", wage);
+                return result;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the hours Worked in a week:  "); // or Console.Write("Type any number:  "); to enter number in the same line
@@ -14,6 +36,10 @@ namespace Employee
             String result = string.Format("{0:F2}", wage);
             Console.WriteLine("Your weekely wage is £: {0} ", result);
             Console.ReadKey();
+
+
+
+
 
         }
     }
