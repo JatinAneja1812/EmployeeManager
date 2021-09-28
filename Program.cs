c6dccb66b41eabefe11d4bec18c7506a7336fc84
W9359412 <79636802+W9359412@users.noreply.github.com>
2021-09-28 07:43:16 +0100

    feature 2 - staff increment to higher wage £14.24  on every extra hours worked after 40 hrs is working

--- Program.cs
+++ Program.cs
@@ -24,20 +24,26 @@ namespace Employee
             }
         }
 
-
+         public static void weekly_wage_calculator(Double hrs)
+        {
+            Double wage = hrs * 9.50;
+            wage = Math.Round(wage, 4, MidpointRounding.AwayFromZero);
+            String result = string.Format("{0:F2}", wage);
+            Console.WriteLine("Your weekely wage is £: {0} ", result);
+            Console.ReadKey();
+        }
 
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the hours Worked in a week:  "); // or Console.Write("Type any number:  "); to enter number in the same line
             Double x = double.Parse(Console.ReadLine());
+            weekly_wage_calculator(x);
 
-            Double wage = x * 9.50;
-            wage = Math.Round(wage, 4, MidpointRounding.AwayFromZero);
-            String result = string.Format("{0:F2}", wage);
-            Console.WriteLine("Your weekely wage is £: {0} ", result);
+            Console.WriteLine("Enter the staff hours Worked in a week :  "); // if staff hours are more than 40 hrs , they will get an increment of £14.24 for extra hour
+            Double y = double.Parse(Console.ReadLine());
+            String result = staff_increment(y);
+            Console.WriteLine("Staff's weekely wage is £: {0} ", result);
             Console.ReadKey();
-
-
 
 
 
