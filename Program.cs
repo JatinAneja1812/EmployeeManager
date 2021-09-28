63825ec2547a099eefb24510fea290bba0bd9087
W9359412 <79636802+W9359412@users.noreply.github.com>
2021-09-28 08:16:31 +0100

    adding employee info working

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace Employee
 {
-    class Program
+    class EmployeeManager
     {
 
 
-        public static String staff_increment(Double hrs)
+        public static String CalcWage(Double hrs)
         {
             if (hrs > 40)
             {
@@ -24,26 +24,23 @@ namespace Employee
             }
         }
 
-         public static void weekly_wage_calculator(Double hrs)
-        {
-            Double wage = hrs * 9.50;
-            wage = Math.Round(wage, 4, MidpointRounding.AwayFromZero);
-            String result = string.Format("{0:F2}", wage);
-            Console.WriteLine("Your weekely wage is £: {0} ", result);
-            Console.ReadKey();
-        }
+        
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the hours Worked in a week:  "); // or Console.Write("Type any number:  "); to enter number in the same line
-            Double x = double.Parse(Console.ReadLine());
-            weekly_wage_calculator(x);
 
+            Console.WriteLine("Staff's infomation");
             Console.WriteLine("Enter the staff hours Worked in a week :  "); // if staff hours are more than 40 hrs , they will get an increment of £14.24 for extra hour
             Double y = double.Parse(Console.ReadLine());
-            String result = staff_increment(y);
-            Console.WriteLine("Staff's weekely wage is £: {0} ", result);
+            Console.WriteLine("Enter the Employee Name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter the Employee ID: (e.g. D54) :  ");
+            String id = Console.ReadLine();
+            String result = CalcWage(y);
+
+            Console.WriteLine("Emplyee {0} with Employee ID {1} weekely wage is £: {2} ",name, id, result);
             Console.ReadKey();
+
 
 
 
